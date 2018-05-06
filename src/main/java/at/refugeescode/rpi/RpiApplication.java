2aee280cd2e34318043fc980ae67a1eb9da07ead
nana80802 <miss_nisreen2000@yahoo.com>
2018-05-06 15:58:07 +0200

    add spring structure

---
+++ src/main/java/at/refugeescode/rpi/RpiApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.rpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpiApplication.class, args);
+	}
+}
